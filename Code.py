a69a8123c2de7906334a79c93074c7c6f98fae56
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-08 14:54:45 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -6,3 +6,19 @@ worldmap_chart.add('Visited countries', ['ir','th','de','fr','fi','no','at','au'
 worldmap_chart.add('The bucket list', ['us','in','cn','jp','nz','za'])
 worldmap_chart.render()
 worldmap_chart.render_to_file('Countries.svg')
+
+
+
+
+
+import pygal
+supra = pygal.maps.world.SupranationalWorld()
+supra.add('Asia', [('asia', 1)])
+supra.add('Europe', [('europe', 1)])
+supra.add('Africa', [('africa', 1)])
+supra.add('North america', [('north_america', 1)])
+supra.add('South america', [('south_america', 1)])
+supra.add('Oceania', [('oceania', 1)])
+supra.add('Antartica', [('antartica', 1)])
+supra.render()
+supra.render_to_file('Continents.svg')
